464d95cab78ab9e8958012841b67f977c4ff55d7
Roopendra Talekar <roopendratalekar@gmail.com>
2024-05-09 12:05:04 +0530

    fix imports

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import Oauth from './src/OAuth';
-import OAuthEcho from './src/OAuthEcho';
-import OAuth2 from './src/oauth2';
 
 export default {
     Oauth,
-    OAuthEcho,
-    OAuth2,
 };
 export {
     Oauth,
-    OAuthEcho,
-    OAuth2,
 };
 
